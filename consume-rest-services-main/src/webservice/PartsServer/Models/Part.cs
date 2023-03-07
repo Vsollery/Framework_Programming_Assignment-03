5fb19f7e2906e02529f302ef251da92e5591db1c
Vsollery <vsollery@gmail.com>
2023-03-07 10:31:27 +0700

    commit

---
+++ consume-rest-services-main/src/webservice/PartsServer/Models/Part.cs
@@ -0,0 +1,13 @@
+﻿namespace PartsService.Models
+{
+    // part provided by the REST web ervice.
+    public class Part
+    {
+        public string PartID { get; set; }
+        public string PartName { get; set; }
+        public List<string> Suppliers { get; set; }
+        public DateTime PartAvailableDate { get; set; }
+        public string PartType { get; set; }
+        public string Href => $"api/parts/{PartID}";
+    }
+}
